88e8b24fb0260ad946e4fc0794d1a7a05fafafe5
David P. Riedel <davidp.riedel@gmail.com>
2021-08-03 10:09:06 -0400

    get first charts working
    - just code inline in test.

--- Unit_Test.cpp
+++ Unit_Test.cpp
@@ -624,14 +624,14 @@ class PlotChartsWithChartDirector : public Test
 
 };
 
-TEST_F(PlotChartsWithChartDirector, Plot10X3Chart)
+TEST_F(PlotChartsWithChartDirector, Plot10X2Chart)
 {
     std::string data = {"1100 1105 1110 1112 1118 1120 1136 1121 1129 1120 1139 1121 1129 1138 1113 1139 1123 1128 1136 1111 1095 1102 1108 1092 1129 "};
     data += "1122 1133 1125 1139 1105 1132 1122 1131 1127 1138 1111 1122 1111 1128 1115 1117 1120 1119 1132 1133 1147 1131 1159 1136 1127";
 
     std::stringstream prices{data, std::ios_base::in}; 
 
-    P_F_Data chart("GOOG", 10, 3);
+    P_F_Data chart("GOOG", 10, 2);
     chart.LoadData<int32_t>(&prices);
 
     EXPECT_EQ(chart.GetCurrentDirection(), P_F_Column::Direction::e_down);
@@ -642,6 +642,38 @@ TEST_F(PlotChartsWithChartDirector, Plot10X3Chart)
     EXPECT_EQ(chart[5].GetHadReversal(), false);
 
     chart.ExportData(&std::cout);
+
+    // for plotting, need arrays of high, low, open, close, labels.
+    // for each column, if direction is down:
+    //   high and open = top
+    //   low and close = bottom 
+    //
+    // if direction is up:
+    //   low and open = bottom
+    //   high and close = top 
+
+    std::vector<double> highData;
+    std::vector<double> lowData;
+    std::vector<double> openData;
+    std::vector<double> closeData;
+
+    for (const auto& col : chart)
+    {
+        if (col.GetDirection() == P_F_Column::Direction::e_up)
+        {
+            lowData.push_back(col.GetBottom());
+            openData.push_back(col.GetBottom());
+            highData.push_back(col.GetTop());
+            closeData.push_back(col.GetTop());
+        }
+        else
+        {
+            highData.push_back(col.GetTop());
+            openData.push_back(col.GetTop());
+            lowData.push_back(col.GetBottom());
+            closeData.push_back(col.GetBottom());
+        }
+    }
 
     XYChart* c = new XYChart(600, 350);
 
@@ -670,15 +702,97 @@ TEST_F(PlotChartsWithChartDirector, Plot10X3Chart)
 
     // Add a CandleStick layer to the chart using green (00ff00) for up candles and red (ff0000) for
     // down candles
-//    CandleStickLayer* layer = c->addCandleStickLayer(DoubleArray(highData, highData_size),
-//        DoubleArray(lowData, lowData_size), DoubleArray(openData, openData_size), DoubleArray(
-//        closeData, closeData_size), 0x00ff00, 0xff0000);
+    CandleStickLayer* layer = c->addCandleStickLayer(DoubleArray(highData.data(), highData.size()),
+        DoubleArray(lowData.data(), lowData.size()), DoubleArray(openData.data(), openData.size()), DoubleArray(
+        closeData.data(), closeData.size()), 0x00ff00, 0xff0000);
 
     // Set the line width to 2 pixels
-//    layer->setLineWidth(2);
+    layer->setLineWidth(2);
 
     // Output the chart
     c->makeChart("/tmp/candlestick.png");
+
+    //free up resources
+    delete c;
+}
+
+TEST_F(PlotChartsWithChartDirector, ProcessFileWithFractionalData)
+{
+    const fs::path file_name{"./test_files/AAPL_close.dat"};
+
+    std::ifstream prices{file_name};
+
+    P_F_Data chart("AAPL", 2, 2);
+    chart.LoadData<DDecDouble>(&prices);
+
+    EXPECT_EQ(chart.GetCurrentDirection(), P_F_Column::Direction::e_down);
+    EXPECT_EQ(chart.GetNumberOfColumns(), 6);
+
+    EXPECT_EQ(chart[5].GetTop(), 1140);
+    EXPECT_EQ(chart[5].GetBottom(), 1130);
+    EXPECT_EQ(chart[5].GetHadReversal(), false);
+
+    chart.ExportData(&std::cout);
+
+    std::vector<double> highData;
+    std::vector<double> lowData;
+    std::vector<double> openData;
+    std::vector<double> closeData;
+
+    for (const auto& col : chart)
+    {
+        if (col.GetDirection() == P_F_Column::Direction::e_up)
+        {
+            lowData.push_back(col.GetBottom());
+            openData.push_back(col.GetBottom());
+            highData.push_back(col.GetTop());
+            closeData.push_back(col.GetTop());
+        }
+        else
+        {
+            highData.push_back(col.GetTop());
+            openData.push_back(col.GetTop());
+            lowData.push_back(col.GetBottom());
+            closeData.push_back(col.GetBottom());
+        }
+    }
+
+    XYChart* c = new XYChart(600, 350);
+
+    // Set the plotarea at (50, 25) and of size 500 x 250 pixels. Enable both the horizontal and
+    // vertical grids by setting their colors to grey (0xc0c0c0)
+    c->setPlotArea(50, 25, 500, 250)->setGridColor(0xc0c0c0, 0xc0c0c0);
+
+    // Add a title to the chart
+    c->addTitle("Universal Stock Index on Jan 2001");
+
+    // Add a custom text at (50, 25) (the upper left corner of the plotarea). Use 12pt Arial
+    // Bold/blue (4040c0) as the font.
+    c->addText(50, 25, "(c) Global XYZ ABC Company", "Arial Bold", 12, 0x4040c0);
+
+    // Add a title to the x axis
+    c->xAxis()->setTitle("Jan 2001");
+
+    // Set the labels on the x axis. Rotate the labels by 45 degrees.
+//    c->xAxis()->setLabels(StringArray(labels, labels_size))->setFontAngle(45);
+
+    // Add a title to the y axis
+    c->yAxis()->setTitle("Universal Stock Index");
+
+    // Draw the y axis on the right hand side of the plot area
+    c->setYAxisOnRight(true);
+
+    // Add a CandleStick layer to the chart using green (00ff00) for up candles and red (ff0000) for
+    // down candles
+    CandleStickLayer* layer = c->addCandleStickLayer(DoubleArray(highData.data(), highData.size()),
+        DoubleArray(lowData.data(), lowData.size()), DoubleArray(openData.data(), openData.size()), DoubleArray(
+        closeData.data(), closeData.size()), 0x00ff00, 0xff0000);
+
+    // Set the line width to 2 pixels
+    layer->setLineWidth(2);
+
+    // Output the chart
+    c->makeChart("/tmp/candlestick2.png");
 
     //free up resources
     delete c;
