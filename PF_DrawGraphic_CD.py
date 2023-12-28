294dc42f7d03b95c87350f7878ea5a2191d2d48c
David P. Riedel <davidp.riedel@gmail.com>
2023-12-28 16:38:23 -0500

    testing: pythong driver to get chart graphics working

---
+++ PF_DrawGraphic_CD.py
@@ -0,0 +1,424 @@
+#!/usr/local/python-3.12/bin/python3
+
+
+"""
+    This is a driver module to read and extract the data from a
+    PF_Chart data file and then plot it as an svg.
+"""
+
+import argparse
+import datetime
+import itertools
+import json
+import logging
+import numpy as np
+import os
+import pandas as pd
+import random
+import sys
+import traceback
+
+from pychartdir import *
+
+import PF_DrawChart_CD
+
+THE_LOGGER = logging.getLogger()        # use the default 'root' name
+THE_LOGGER.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+THE_LOGGER.addHandler(handler)
+
+sys.path.insert(0, os.path.expanduser("~/projects/PF_Project/PY_PF_Chart"))
+import PY_PF_Chart
+
+def Main():
+
+    result = 0
+
+    try:
+
+        args = GetArgs()
+
+        if makes_sense_to_run(args):
+            ProcessChartFile(args)
+        else:
+            print("Unable to create graphic.")
+            result = 2
+
+    # parse_args will throw a SystemExit after displaying help info.
+
+    except SystemExit:
+        result = 7
+        pass
+
+    except Exception:
+
+        traceback.print_exc()
+        sys.stdout.flush()
+        result = 6
+
+    return result
+
+
+def GetArgs():
+
+    parser = argparse.ArgumentParser(description='Draw the graphic for the specified PF_Chart file.')
+    parser.add_argument("-f", "--file", action="store", dest="input_file_name_", required=True,
+                        help="Path name of file to process.")
+    parser.add_argument("-o", "--output-dir", action="store", dest="output_directory_name_", default="/tmp",
+                        help="Path name of directory to write output file to. Default is '/tmp'.")
+    parser.add_argument("--format", action="store", dest="y_axis_format_", required=True,
+                        help="Use 'time' or 'date' for y-axis labels.")
+    parser.add_argument("-t", "--trend-lines", action="store", dest="trend_lines_", default="no",
+                        help="Draw trend lines on graphic. Default is 'no'. Can be 'data' or 'angle'.")
+    parser.add_argument("-n", "--number-cols", type=int, action="store", dest="number_columns_", default=0,
+                        help="Maximun number of columns to show in graph. Default is '0' which means 'all'.")
+    parser.add_argument("--prices", action="store", dest="prices_file_",
+                        help="Path name of file containing 'streamed' price data.")
+    parser.add_argument("-l", "--logging", action="store", dest="log_level_", default="warning",
+                        help="logging level: info, debug, warning, error, critical. Default is 'warning'.")
+    parser.add_argument("-u", "--user", action="store", dest="user_name_", default="data_updater_pg", required=False,
+                        help="which DB user to run as. Default is 'data_updater_pg'.")
+    parser.add_argument("-d", "--database", action="store", dest="database_name_", default="finance", required=False,
+                        help="which DB to connect to. Default is 'finance'.")
+    parser.add_argument("-m", "--machine_", action="store", dest="machine_name_", default="localhost", required=False,
+                        help="machine name or IP of computer whose database we are using. Default is 'localhost'.")
+    parser.add_argument("--DB_port", action="store", dest="DB_port_", default="5432", required=False,
+                        help="Postgres port number for source DB. Default: 5432")
+
+    # args = parser.parse_args(["-b2017-04-13", "-e2017-04-13", "-k"])
+    # args = parser.parse_args(["-b2017-12-25", "-e2017-12-25"])
+    args = parser.parse_args()
+
+    LEVELS = {"debug": logging.DEBUG,
+              "info": logging.INFO,
+              "warning": logging.WARNING,
+              "error": logging.ERROR,
+              "critical": logging.CRITICAL,
+              "none": 9999}
+    log_level = LEVELS.get(args.log_level_, logging.WARNING)
+    THE_LOGGER.setLevel(level=log_level)
+    if log_level == 9999:
+        logging.getLogger().addHandler(logging.NullHandler())
+
+    return args
+
+
+def makes_sense_to_run(args):
+
+    if not os.path.exists(args.input_file_name_):
+        print("Unable to find specified file: %s" % args.input_file_name_)
+        return False
+
+    if (args.y_axis_format_ != "date" and args.y_axis_format_ != "time"):
+        print("Format: %s must be either: 'date' or 'time'.")
+        return False
+
+    if (args.trend_lines_ != "no" and args.trend_lines_ != "data" and args.trend_lines_ != "angle"):
+        print("Trend lines: %s must be either: 'no' or 'data' or 'angle'.")
+        return False
+
+    return True
+
+
+def ProcessChartFile(args):
+
+    # define some colors
+
+    RED = 0xFF0000
+    GREEN = 0x00FF00
+    BLUE = 0x0000FF
+    ORANGE = 0xFFA500
+
+    chart_data = PY_PF_Chart.PY_PF_Chart.MakeChartFromJSONFile(args.input_file_name_)
+
+    up_columns = chart_data.GetBoxesForColumns(PY_PF_Chart.PF_ColumnFilter.e_up_column)
+    down_columns = chart_data.GetBoxesForColumns(PY_PF_Chart.PF_ColumnFilter.e_down_column)
+    reversed_to_up_columns = chart_data.GetBoxesForColumns(PY_PF_Chart.PF_ColumnFilter.e_reversed_to_up)
+    reversed_to_down_columns_columns = chart_data.GetBoxesForColumns(PY_PF_Chart.PF_ColumnFilter.e_reversed_to_down)
+    # print(up_columns)
+
+    upcol_pd = pd.DataFrame(up_columns, columns=["col_nbr", "price"])
+    downcol_pd = pd.DataFrame(down_columns, columns=["col_nbr", "price"])
+    rev_to_up_pd = pd.DataFrame()
+    if chart_data.GetReversalBoxes() == 1:
+        rev_to_up_pd = pd.DataFrame(reversed_to_up_columns, columns=["col_nbr", "price"])
+    rev_to_down_pd = pd.DataFrame()
+    if chart_data.GetReversalBoxes() == 1:
+        rev_to_down_pd = pd.DataFrame(reversed_to_down_columns_columns, columns=["col_nbr", "price"])
+
+    first_col = chart_data.GetColumn(0)
+    openning_price = first_col.GetColumnBottom() if first_col.GetColumnDirection() == PY_PF_Chart.PY_PF_Column.Direction.e_Up else first_col.GetColumnTop() 
+    print(openning_price)
+
+    overall_pct_chg = .5    # TODO: place holder
+    explanation_text = ""
+    if chart_data.HasReversedColumns():
+        explanation_text = "Orange: 1-step Up then reversal Down. Green: 1-step Down then reversal Up."
+
+    skipped_columns_text = ""
+    skipped_columns = 5     #   TODO: place holder
+    max_columns_for_graph = 100     #   TODO place holder
+
+    if skipped_columns > 0:
+        skipped_columns_text = " (last {} cols)".format(max_columns_for_graph)
+
+    chart_title = "\n{}{} X {} for {} {}. Overall % change: {}{}\nLast change: {:%Y-%m-%d %H:%M:%S}\n{}".format(
+        chart_data.GetBoxSize(), ("%" if chart_data.IsPercent() else ""), chart_data.GetReversalBoxes(),
+        chart_data.GetSymbol(), ("percent" if chart_data.IsPercent() else ""), overall_pct_chg, skipped_columns_text, datetime.datetime.fromtimestamp(int(chart_data.GetLastChangeTimeSeconds().total_seconds())),
+        explanation_text)
+
+    print(chart_title)
+
+    x_axis_labels = []
+
+    for col in chart_data:
+        xxx = "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.fromtimestamp(int(col.GetColumnBeginTime().total_seconds())))
+        x_axis_labels.append(xxx)
+
+    c = XYChart(1450, 1520)
+
+    # Set the plotarea at (55, 65) and of size 350 x 300 pixels, with a light grey border (0xc0c0c0).
+    # Turn on both horizontal and vertical grid lines with light grey color (0xc0c0c0)
+    c.setPlotArea(55, 125, 1350, 1300, -1, -1, 0xc0c0c0, 0xc0c0c0, -1)
+
+    # Add a legend box at (50, 30) (top of the chart) with horizontal layout. Use 12pt Times Bold Italic
+    # font. Set the background and border color to Transparent.
+    # c.addLegend(50, 30, 0, "Times New Roman Bold Italic", 12).setBackground(Transparent)
+
+    # Add a title to the chart using 18pt Times Bold Itatic font.
+    c.addTitle(chart_title, "Times New Roman Bold Italic", 18)
+
+    # Add a title to the y axis using 12pt Arial Bold Italic font
+    # c.yAxis().setTitle("Length (cm)", "Arial Bold Italic", 12)
+
+    # Add a title to the x axis using 12pt Arial Bold Italic font
+    # c.xAxis().setTitle("Weight (kg)", "Arial Bold Italic", 12)
+
+    # Set the axes line width to 3 pixels
+    c.xAxis().setWidth(3)
+    c.yAxis().setWidth(3)
+
+    # Add an orange (0xff9933) scatter chart layer, using 13 pixel diamonds as symbols
+    c.addScatterLayer(upcol_pd.col_nbr, upcol_pd.price, "first chart", Cross2Shape(.5), 13, GREEN)
+    c.addScatterLayer(downcol_pd.col_nbr, downcol_pd.price, "first chart", CircleShape, 13, RED)
+    if not rev_to_up_pd.empty:
+        c.addScatterLayer(rev_to_up_pd.col_nbr, rev_to_up_pd.price, "first chart", Cross2Shape(.5), 13,BLUE)
+    if not rev_to_down_pd.empty:
+        c.addScatterLayer(rev_to_down_pd.col_nbr, rev_to_down_pd.price, "first chart", CircleShape, 13, ORANGE)
+
+    # Add a green (0x33ff33) scatter chart layer, using 11 pixel triangles as symbols
+    # c.addScatterLayer(dataX1, dataY1, "Natural", TriangleSymbol, 11, 0x33ff33)
+
+    layer1 = c.addLineLayer([openning_price, openning_price], RED)
+    layer1.setXData([0, chart_data.GetNumberOfColumns()])
+    layer1.setLineWidth(2)
+    # Output the chart
+
+    yyy = c.xAxis().setLabels(x_axis_labels)
+    c.xAxis().setLabelStep(int(chart_data.GetNumberOfColumns() / 40), 0)
+    yyy.setFontAngle(-45)
+
+    c.makeChart("scatter.svg")
+
+
+    sys.exit()
+
+
+
+
+    openData = []
+    closeData = []
+    highData = []
+    lowData = []
+    had_step_back = []
+    direction_is_up = []
+    x_axis_labels = []
+
+    first_col = None
+    if len(chart_data["columns"]) > 0:
+        first_col = chart_data["columns"][0]
+    else:
+        first_col = chart_data["current_column"]
+
+    openning_price = first_col["bottom"] if first_col["direction"] == "up" else first_col["top"]
+
+    for col in chart_data["columns"]:
+        lowData.append(float(col["bottom"]))
+        highData.append(float(col["top"]))
+
+        if col["direction"] == "up":
+            openData.append(float(col["bottom"]))
+            closeData.append(float(col["top"]))
+            direction_is_up.append(True)
+        else:
+            openData.append(float(col["top"]))
+            closeData.append(float(col["bottom"]))
+            direction_is_up.append(False)
+
+        # need to do proper time selection here
+        if (args.y_axis_format_ == "date"):
+            x_axis_labels.append(datetime.datetime.fromtimestamp(int(col["first_entry"]) / 1e9).date())
+        else:
+            x_axis_labels.append(datetime.datetime.fromtimestamp(int(col["first_entry"]) / 1e9))
+        had_step_back.append(col["had_reversal"])
+
+    lowData.append(float(chart_data["current_column"]["bottom"]))
+    highData.append(float(chart_data["current_column"]["top"]))
+
+    if chart_data["current_column"]["direction"] == "up":
+        openData.append(float(chart_data["current_column"]["bottom"]))
+        closeData.append(float(chart_data["current_column"]["top"]))
+        direction_is_up.append(True)
+    else:
+        openData.append(float(chart_data["current_column"]["top"]))
+        closeData.append(float(chart_data["current_column"]["bottom"]))
+        direction_is_up.append(False)
+
+    # need to do proper time selection here
+    if (args.y_axis_format_ == "date"):
+        x_axis_labels.append(datetime.datetime.fromtimestamp(int(chart_data["current_column"]["first_entry"]) / 1e9).date())
+    else:
+        x_axis_labels.append(datetime.datetime.fromtimestamp(int(chart_data["current_column"]["first_entry"]) / 1e9))
+
+    had_step_back.append(chart_data["current_column"]["had_reversal"])
+
+    # see if we need to grab n most recent columns
+
+    if args.number_columns_ > 0:
+        x_axis_labels = x_axis_labels[:-args.number_columns_]
+        direction_is_up = direction_is_up[:-args.number_columns_]
+        had_step_back = had_step_back[:-args.number_columns_]
+        openData = openData[:-args.number_columns_]
+        closeData = closeData[:-args.number_columns_]
+        highData = highData[:-args.number_columns_]
+        lowData = lowData[:-args.number_columns_]
+
+    chart_title = "just testing"
+
+    the_data = {}
+    the_data["Date"] = x_axis_labels
+    the_data["Open"] = openData
+    the_data["High"] = highData
+    the_data["Low"] = lowData
+    the_data["Close"] = closeData
+
+    the_signals = {}
+
+    # NOTE: this code is 'copied' from my C++ code which ported to Python pretty
+    # directly (ignoring syntax)
+
+    signal_data = chart_data["signals"]
+
+    # need to have correct number of values in list
+    # must be same as number of rows columns in data
+    dt_buys = [np.nan] * len(the_data["Open"])
+    tt_buys = [np.nan] * len(the_data["Open"])
+    db_sells = [np.nan] * len(the_data["Open"])
+    tb_sells = [np.nan] * len(the_data["Open"])
+    bullish_tt_buys = [np.nan] * len(the_data["Open"])
+    bearish_tb_sells = [np.nan] * len(the_data["Open"])
+    cat_buys = [np.nan] * len(the_data["Open"])
+    cat_sells = [np.nan] * len(the_data["Open"])
+    tt_cat_buys = [np.nan] * len(the_data["Open"])
+    tb_cat_sells = [np.nan] * len(the_data["Open"])
+
+    had_dt_buy = 0
+    had_tt_buy = 0
+    had_db_sell = 0
+    had_tb_sell = 0
+    had_bullish_tt_buy = 0
+    had_bearish_tb_sell = 0
+    had_cat_buy = 0
+    had_cat_sell = 0
+    had_tt_cat_buy = 0
+    had_tb_cat_sell = 0
+
+    for col_num, sigs in itertools.groupby(signal_data, lambda s: s["column"]):
+        most_important = max(sigs, key=lambda s: int(s["priority"]))
+
+        match most_important["type"]:
+            case "dt_buy":
+                dt_buys[int(most_important["column"])] = float(most_important["box"])
+                had_dt_buy += 1
+            case "db_sell":
+                db_sells[int(most_important["column"])] = float(most_important["box"])
+                had_db_sell += 1
+            case "tt_buy":
+                tt_buys[int(most_important["column"])] = float(most_important["box"])
+                had_tt_buy += 1
+            case "tb_sell":
+                tb_sells[int(most_important["column"])] = float(most_important["box"])
+                had_tb_sell += 1
+            case "bullish_tt_buy":
+                bullish_tt_buys[int(most_important["column"])] = float(most_important["box"])
+                had_bullish_tt_buy += 1
+            case "bearish_tb_sell":
+                bearish_tb_sells[int(most_important["column"])] = float(most_important["box"])
+                had_bearish_tb_sell += 1
+            case "catapult_buy":
+                cat_buys[int(most_important["column"])] = float(most_important["box"])
+                had_cat_buy += 1
+            case "catapult_sell":
+                cat_sells[int(most_important["column"])] = float(most_important["box"])
+                had_cat_sell += 1
+            case "ttop_catapult_buy":
+                tt_cat_buys[int(most_important["column"])] = float(most_important["box"])
+                had_tt_cat_buy += 1
+            case "tbot_catapult_sell":
+                tb_cat_sells[int(most_important["column"])] = float(most_important["box"])
+                had_tb_cat_sell += 1
+
+    the_signals["dt_buys"] = dt_buys if had_dt_buy else []
+    the_signals["db_sells"] = db_sells if had_db_sell else []
+    the_signals["tt_buys"] = tt_buys if had_tt_buy else []
+    the_signals["tb_sells"] = tb_sells if had_tb_sell else []
+    the_signals["bullish_tt_buys"] = bullish_tt_buys if had_bullish_tt_buy else []
+    the_signals["bearish_tb_sells"] = bearish_tb_sells if had_bearish_tb_sell else []
+    the_signals["catapult_buys"] = cat_buys if had_cat_buy else []
+    the_signals["catapult_sells"] = cat_sells if had_cat_sell else []
+    the_signals["tt_catapult_buys"] = cat_buys if had_cat_buy else []
+    the_signals["tb_catapult_sells"] = cat_sells if had_cat_sell else []
+
+    streamed_prices = {}
+    streamed_prices["the_time"] = []
+    streamed_prices["price"] = []
+    streamed_prices["signal_type"] = []
+
+    if args.prices_file_:
+        prices = pd.read_csv(args.prices_file_, usecols=[0, 1], parse_dates=[0])
+
+        streamed_prices["the_time"] = prices["date"].astype(np.int64)
+        streamed_prices["price"] = prices["close"]
+        streamed_prices["signal_type"] = [0] * len(streamed_prices["price"])
+
+        # generate some random 'signals' just for testing purposes.
+
+        for ndx in range(20):
+            streamed_prices["signal_type"][random.choice(range(len(streamed_prices["signal_type"])))] = 4
+        for ndx in range(20):
+            streamed_prices["signal_type"][random.choice(range(len(streamed_prices["signal_type"])))] = 5
+
+    date_time_format = "%Y-%m-%d" if args.y_axis_format_ == "date" else "%H:%M:%S"
+
+    chart_name = MakeChartName(chart_data)
+    graphic_file_name = os.path.join(args.output_directory_name_, chart_name)
+
+    PF_DrawChart_prices.DrawChart(the_data, chart_data["current_column"]["reversal_boxes"], direction_is_up,
+                                  had_step_back, chart_title, graphic_file_name, date_time_format,
+                                  args.trend_lines_, False, float(chart_data["y_min"]),
+                                  float(chart_data["y_max"]), float(openning_price), the_signals, streamed_prices)
+
+
+def MakeChartName(chart_data):
+    chart_name = "{}_{}{}X{}_{}.{}".format(chart_data["symbol"], chart_data["fname_box_size"],
+                                           ("%" if chart_data["boxes"]["box_scale"] == "percent" else ""),
+                                           chart_data["current_column"]["reversal_boxes"],
+                                           ("linear" if chart_data["boxes"]["box_scale"] == "linear"
+                                            else "percent"), "svg")
+    return chart_name
+
+
+if __name__ == '__main__':
+    sys.exit(Main())
